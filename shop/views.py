2cc2eb5ce80619b4e9c4012ddd0619d97b49919f
Faiz <ahmadfaiz2006@gmail.com>
2020-05-11 17:38:50 +0530

    creating a category wise products

--- shop/views.py
+++ shop/views.py
@@ -6,10 +6,19 @@ from math import ceil
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
+   
     # paramssd = {'no_of_slides':nSlides, "product":products, 'range':range(1,nSlides)}
-    allProds = [[products,range(1,nSlides),nSlides],[products,range(1,nSlides),nSlides]]
+    # allProds = [[products,range(1,nSlides),nSlides],[products,range(1,nSlides),nSlides]]
+    
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(products)
+        nSlides = n//4 + ceil((n/4)-(n//4))
+        allProds.append([prod,range(1,nSlides),nSlides])
+        
     params = {'allProds':allProds}
     return render(request,'shop/index.html',params)
 
