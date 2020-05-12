8e18dca3c0cc346f0053fd632b59d7aba6455d49
Faiz <ahmadfaiz2006@gmail.com>
2020-05-13 01:14:25 +0530

    Add popover using bootstrap

--- shop/views.py
+++ shop/views.py
@@ -26,16 +26,16 @@ def about(request):
     return render(request,'shop/about.html')
 
 def contact(request):
-    return HttpResponse("we r at contact")
+    return render(request,'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request,'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("we are at search")
+    return render(request,'shop/search.html')
 
 def productView(request):
-    return HttpResponse("We are at ProductView")
+    return render(request,'shop/prodView.html')
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request,'shop/checkout.html')
