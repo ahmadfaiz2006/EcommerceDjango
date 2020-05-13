c0bc4edd04830f5097e29def455a272363960f6d
Faiz <ahmadfaiz2006@gmail.com>
2020-05-14 01:37:24 +0530

    Creating ContactUS

--- shop/models.py
+++ shop/models.py
@@ -13,3 +13,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class Contact(models.Model):
+    contactid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 50, default = "")
+    phone = models.IntegerField(default=0)
+    desc = models.CharField(max_length = 500, default="") 
+    
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 
 # Create your views here.
@@ -23,9 +23,18 @@ def index(request):
     return render(request,'shop/index.html',params)
 
 def about(request):
+    
     return render(request,'shop/about.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name',"")
+        email = request.POST.get('email',"")
+        phone = request.POST.get('phone',"")
+        desc = request.POST.get('desc',"")
+        # print(name,email,phone,desc)
+        contact = Contact(name = name,email=email,phone=phone,desc=desc)
+        contact.save()
     return render(request,'shop/contact.html')
 
 def tracker(request):
